607ee8f1f3918f351643affc0ab2371ab77efa27
SJohnRose <shamini.johnrose@gmail.com>
2023-04-10 10:53:31 +1000

    created user routes and get all users controller

---
+++ controllers/userController.js
@@ -0,0 +1,21 @@
+const { ObjectId } = require('mongoose').Types;
+const { User, Thought } = require('../models');
+
+
+module.exports = {
+  // Get all users
+  getUsers(req, res) {
+    User.find()
+      .then(async (users) => {
+        const userObj = {
+          users,
+          
+        };
+        return res.json(userObj);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+};
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+  removeThought,
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).delete(deleteUser).put(updateUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+// /api/users/:userId/thoughts/:thoughtId
+router.route('/:userId/thoughts/:thoughtId').delete(removeThought);
+
+module.exports = router;
