3553b903b9e66e75e2cb9910e839f772d318ca4a
SJohnRose <shamini.johnrose@gmail.com>
2023-04-14 11:49:57 +1000

    added code to update single user

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -15,13 +15,12 @@ router.route('/').get(getUsers).post(createUser);
 
 
 // /api/users/:userId
-// router.route('/:userId').get(getSingleUser).delete(deleteUser).put(updateUser);
-router.route('/:userId').get(getSingleUser).delete(deleteUser);
+router.route('/:userId').get(getSingleUser).delete(deleteUser).put(updateUser);
+
 
 // /api/users/:userId/friends/:friendId
 // router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
-// /api/users/:userId/thoughts/:thoughtId
-// router.route('/:userId/thoughts/:thoughtId').delete(removeThought);
+
 
 module.exports = router;
